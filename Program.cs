21a52d077921c661b2343d65b78a6792dcb8a942
Ex7ARZ <56121801+Ex7ARZ@users.noreply.github.com>
2020-03-28 11:05:29 +0300

    lab5
    lab5

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using LAB3;
 
-internal class Human
+public class Human
 {
     private static int _id;
     private readonly string _sex;
@@ -29,7 +30,9 @@ internal class Human
         return
             $"Name:{Name} \n Sex:{_sex} \n Height:{_height} cm \n Weight={_weight} kg \n Age={Age} ya \n Country:{Country} \n id={_id}";
     }
-
+   public string RetName()=>Name;
+   public int RetId() => _id;
+   public string RetCountry() => Country;
     public void HumanStats(int height, int weight, int age)
     {
         _height = height;
@@ -38,32 +41,67 @@ internal class Human
     }
 }
 
-internal class Humanity
-{
-    private readonly Human _people1 = new Human(1, 2, 3, "john", "5", "1");
-    private readonly Human _people2 = new Human(8, 8, 3, "not john", "5", "1");
-
-    public void MenuStart()
+ class Humanity
+ {
+   
+   Student Programmers=new Programmer(20,180,80,"John","USA","man",3,"BSUIR");
+    public unsafe void MenuStart()
     {
         var m = true;
         while (m)
         {
-            Console.Write("change-1\nshow human-2\nend program-3\n");
+            Console.Write("change-1\nshow human-2\nend program-3\n new-4\n go study -5\n show university-6\n");
             var s = Console.ReadLine();
             var index = int.Parse(s);
+            
             switch (index)
             {
                 case 1:
-                    _people1.HumanStats(20, 20, 20);
+                {
+                    Console.WriteLine( Programmers.HumanStats());
                     break;
+                }
                 case 2:
-                    Console.WriteLine(_people1.HumanStats());
-                    Console.WriteLine(_people2.HumanStats());
+                    Console.WriteLine(Programmers.HumanStats());
+                    Console.WriteLine(Programmers.HumanStats());
                     break;
                 case 3:
                     m = false;
                     break;
+                case 4:
+                    
+                    break;
+                 case 5:
+                      Programmers.GoStudy();
+                      break;
+                  case 6:
+                      Console.Clear();
+                      Console.WriteLine(Programmers.Study());
+                      Console.WriteLine(Programmers.MyMarks());
+                      Console.Write("\n");
+                      break;
             }
+        }
+       
+    }
+
+    public void ChooseOne()
+    {
+        int n = Console.Read();
+        Console.Clear();
+        Console.WriteLine("1-name \n 2-country\n3-persinal id");
+        switch (n)
+        {
+            case 1:
+                Console.WriteLine(Programmers.RetName()+"\n");
+                break;
+                case 2:
+                    Console.WriteLine($"{Programmers.RetId()}\n");
+                    break;
+                    case 3:
+                        Console.WriteLine(Programmers.RetCountry()+"\n");
+                        break;
+                    
         }
     }
 }
@@ -72,6 +110,7 @@ namespace LAB3
 {
     internal class Program
     {
+        
         private static void Main(string[] args)
         {
             var first = new Humanity();
@@ -79,4 +118,3 @@ namespace LAB3
         }
     }
 }
-//r3tt
---
+++ Student.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+
+namespace LAB3
+{
+    
+    public enum Course
+    {
+        Entrant,
+        First,
+        Second,
+        Third,
+        Fourth
+    };
+
+    public abstract class Student:    Human
+    {
+       
+        protected string Uneversity;
+        
+        private Course S;
+   
+        public Student(int age, int height, int weight, string name, string country, string sex,int course,string un ) : base(age, height, weight, name, country, sex)
+   {
+       S = (Course) course;
+       Uneversity = un;
+   }
+
+        public virtual void GoStudy()
+        {
+            Console.WriteLine("I am studying!");
+        }
+   public string Study() => $"{Uneversity}  {S} Course \n";
+   public abstract string MyMarks();
+    }
+    
+}
